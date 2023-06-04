58b20b4d954311db2cc67a1b2e07400e40fb504e
Ohad Cohen <ohad.cohen@biu.ac.il>
2023-06-04 18:57:50 +0300

    adv recorder

--- config_panel.py
+++ config_panel.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import os
 from tkinter import filedialog
+from record_panel import RecorderWindow
 class AudioRecorderApp:
     def __init__(self):
         self.window = tk.Tk()
@@ -42,22 +43,42 @@ class AudioRecorderApp:
         speaker_id = self.entry_speaker_id.get()
         saved_path = self.create_folder_for_speaker_id(speaker_id)
         self.save_speaker_info(saved_path,speaker_id)
-        self.submit_label.config(text="Recording Speaker ID " + speaker_id)
-        self.window.destroy()
-        self.show_new_window()
+        self.window.withdraw()
+        self.show_new_window(saved_path,speaker_id)
     
     def run(self):
         self.window.mainloop()
     
-    def show_new_window(self):
-        new_window = tk.Tk()
-        new_window.geometry("800x600")
-        new_window.title("New Window")
+    def show_new_window(self,saved_path,speaker_id):
+        global new_window
+        new_window = tk.Toplevel(self.window)
+        new_window.geometry("900x600")
+        new_window.title("Record Panel")
         # Add widgets and customize the new window as needed
-
-        # Call new_window.mainloop() to start the main event loop for the new window
-        new_window.mainloop()
+        # self.submit_label.config(text="Saved in path: " + saved_path)
+        self.submit_frame = tk.Frame(new_window)  # Create a frame to hold the label and button
+        self.submit_frame.pack()
+        self.submit_label = tk.Label(self.submit_frame)
+        self.submit_label.pack(side=tk.BOTTOM)
+        self.submit_label.config(text="Recording Speaker ID " + speaker_id+ "\n Saved in path: " + saved_path)
+        return_button = tk.Button(new_window, text="Return", command=self.return_to_main)
+        return_button.pack(side=tk.BOTTOM)
+        return_button.pack(pady=10)
+        RecorderWindow(new_window)
+        
     
+    def return_to_main(self):
+        self.submit_frame = tk.Frame(self.window)  # Create a frame to hold the label and button
+        self.submit_frame.pack()
+        
+        self.submit_label = tk.Label(self.submit_frame)
+        self.submit_label.pack(side=tk.LEFT)
+        
+        self.button_submit = tk.Button(self.submit_frame, text="Record", command=self.button_click)
+        self.button_submit.pack(side=tk.LEFT)
+        new_window.destroy()
+        self.window.deiconify()
+        
     def save_speaker_info(self,path,id_number):
         gender = self.selected_gender.get()
         if gender == 1:
